94bba72426fa79ad70ca125e8811f93d654404a4
narobinson1 <nicolas.alexander.robinson.2@gmail.com>
2023-07-21 13:01:36 +0200

    Remove incomplete category marker functionality for map

--- app.py
+++ app.py
@@ -12,18 +12,6 @@ import dash_bootstrap_components as dbc
 
 import requests
 
-if 'REDIS_URL' in os.environ:
-    # Use Redis & Celery if REDIS_URL set as an env variable
-    from celery import Celery
-    celery_app = Celery(__name__, broker=os.environ['REDIS_URL'], backend=os.environ['REDIS_URL'])
-    background_callback_manager = CeleryManager(celery_app)
-
-else:
-    # Diskcache for non-production apps when developing locally
-    import diskcache
-    cache = diskcache.Cache("./cache")
-    background_callback_manager = DiskcacheManager(cache)
-    
 
 # Initialize the app - incorporate a Dash Bootstrap theme
 external_stylesheets = [dbc.themes.CERULEAN]
@@ -156,9 +144,7 @@ def update_map(dropdown_input, state):
 @callback(
     Output('output-graph-1', 'figure'),
     Input('dropdown-component-final', 'value'),
-    State('memory-output', 'data'),
-    background=True,
-    manager=background_callback_manager
+    State('memory-output', 'data')
 )
 def update_graph(dropdown_input, state):
     figure = px.bar(get_totals(dropdown_input, state), x='mock_list', y='mock_results')
