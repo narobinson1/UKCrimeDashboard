e82ae6c47e6a85851097abdbb604c350970f6565
narobinson1 <nicolas.alexander.robinson.2@gmail.com>
2023-07-24 16:10:37 +0200

    Change content formatting

--- app.py
+++ app.py
@@ -56,6 +56,49 @@ url_bar_and_content_div = html.Div([
     html.Div(id='page-content')
 ], style={"background-color": COLORS['content-background'], "height":"100vh"})
 
+# Fetch raw data
+@functools.lru_cache()
+def fetch_data(hashable_tuple):
+    print(hashable_tuple)
+    lat_lng = list(hashable_tuple)
+    lat = list(lat_lng[0])
+    lng = list(lat_lng[1])
+    payload = {'lat':lat, 'lng':lng}
+    r = requests.post("https://data.police.uk/api/crimes-street/all-crime", params=payload)
+    res = r.json()
+
+    return res
+
+# Get totals from raw data
+def get_totals(input, json_data):
+    mock_list = input
+    df = pd.read_json(json_data, orient="split")
+    mock_results = []
+    for mock_city in mock_list:
+        inst = df[df['city'] == mock_city]
+        hashable_input = tuple([tuple(inst.lat), tuple(inst.lng)])
+        api_data = fetch_data(hashable_input)
+        df_inst = pd.DataFrame(api_data)
+        mock_results.append(len(df_inst))
+    result = {'mock_list': mock_list, 'mock_results': mock_results}
+    df = pd.DataFrame.from_dict(result)
+    return df
+    
+# Get statistics from raw data
+def get_counts_df(city_value, json_data):
+    df = pd.read_json(json_data, orient="split")
+    inst = df[df['city'] == city_value]
+    hashable_input = tuple([tuple(inst.lat), tuple(inst.lng)])
+    api_data = fetch_data(hashable_input)
+    return pd.DataFrame(api_data)
+   
+def get_counts_json(city_value, json_data):
+    df = pd.read_json(json_data, orient="split")
+    inst = df[df['city'] == city_value]
+    hashable_input = tuple([tuple(inst.lat), tuple(inst.lng)])
+    api_data = fetch_data(hashable_input)
+    return api_data
+    
 
 dashboard_layout = html.Div(
     children=[
@@ -213,32 +256,19 @@ behind_layout = html.Div(
                 
                 html.Div(
                     children=[
-                        dcc.Tabs(
+                        dcc.Markdown(
                             children=[
-                                dcc.Tab(label='Data retrieval', value='tab-1-behind', style=tab_style, selected_style=tab_selected_style),
-                                dcc.Tab(label='Callback function executions', value='tab-2-behind', style=tab_style, selected_style=tab_selected_style),
-                                dcc.Tab(label='Figure update operations', value='tab-3-behind', style=tab_style, selected_style=tab_selected_style),
+                                '''
+                                    ### Python
+                                    * Functools lru_cache: data retrieval function calls with identical arguments are cached using lru_cache. The following data results from your interaction with the dashboard:
+                                        
+                                        *Cache hits: {},*
+                                        *Cache misses: {},*
+                                        *Cache stores: {}*
+                                '''.format(fetch_data.cache_info().hits, fetch_data.cache_info().misses, fetch_data.cache_info().currsize)
                             ],
-                            colors={
-                                'primary':COLORS['content-background'],
-                                'background':COLORS['content-background']
-                            },
-                            value='tab-1-behind',
-                            id='tabs-behind',
+                            style={'color':COLORS['general']}
                         ),
-                        html.Div(
-                            children=[
-                                html.Div(
-                                    children=[
-                                        html.H6('Tab content'),
-                                    ],
-                                    style={"padding-top": "2rem", "color":COLORS['general']},
-                                    id='tabs-content'
-                                ),
-                        
-                            ],
-                            id="graph-loading-1"
-                        )
                     ],
                     style={"padding-left": "3rem", "padding-right": "3rem", "padding-bottom": "3rem", "padding-top": "0rem"}
                 ),
@@ -447,49 +477,6 @@ def update_map(dropdown_input, state):
                 
     return figure
 
-
-# Fetch raw data
-@functools.lru_cache()
-def fetch_data(hashable_tuple):
-    print(hashable_tuple)
-    lat_lng = list(hashable_tuple)
-    lat = list(lat_lng[0])
-    lng = list(lat_lng[1])
-    payload = {'lat':lat, 'lng':lng}
-    r = requests.post("https://data.police.uk/api/crimes-street/all-crime", params=payload)
-    res = r.json()
-
-    return res
-
-# Get totals from raw data
-def get_totals(input, json_data):
-    mock_list = input
-    df = pd.read_json(json_data, orient="split")
-    mock_results = []
-    for mock_city in mock_list:
-        inst = df[df['city'] == mock_city]
-        hashable_input = tuple([tuple(inst.lat), tuple(inst.lng)])
-        api_data = fetch_data(hashable_input)
-        df_inst = pd.DataFrame(api_data)
-        mock_results.append(len(df_inst))
-    result = {'mock_list': mock_list, 'mock_results': mock_results}
-    df = pd.DataFrame.from_dict(result)
-    return df
-    
-# Get statistics from raw data
-def get_counts_df(city_value, json_data):
-    df = pd.read_json(json_data, orient="split")
-    inst = df[df['city'] == city_value]
-    hashable_input = tuple([tuple(inst.lat), tuple(inst.lng)])
-    api_data = fetch_data(hashable_input)
-    return pd.DataFrame(api_data)
-   
-def get_counts_json(city_value, json_data):
-    df = pd.read_json(json_data, orient="split")
-    inst = df[df['city'] == city_value]
-    hashable_input = tuple([tuple(inst.lat), tuple(inst.lng)])
-    api_data = fetch_data(hashable_input)
-    return api_data
     
 if __name__ == '__main__':
     app.run_server(debug=True)
