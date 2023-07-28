4216f226643670a36980574aa21265fd0f60997d
narobinson1 <nicolas.alexander.robinson.2@gmail.com>
2023-07-28 16:30:25 +0200

    Incorporate fractional and month into db

--- db_main.py
+++ db_main.py
@@ -6,76 +6,140 @@ import mysql.connector
 locations = ['London', 'Manchester']
 df = pd.read_csv("gb_latlon.csv")
 
-locations = list(df['city'])[:10]
 
 
-def fetch_data(lat, lng):
-    payload = {'lat':lat, 'lng':lng}
-    r = requests.post("https://data.police.uk/api/crimes-street/all-crime", params=payload)
-    res = r.json()
-    return res
+
+d = []
+
+for x in range(24):
+    l = ["20"]
+    if x < 10:
+        l.append("0")
+        l.append(str(x))
+        l.append("-")
+        l_ = l[:]
+    else:
+        l.append(str(x))
+        l.append("-")
+        l_ = l[:]
+        
+    for j in range(1, 13):
+        if j < 10:
+            l_.append("0")
+            l_.append(str(j))
+            d.append(''.join(l_))
+            l_ = l[:]
+        else:
+            l_.append(str(j))
+            d.append(''.join(l_))
+            l_ = l[:]
+
+def fetch_data(lat, lng, date):
+        payload = {'lat':lat, 'lng':lng, 'date':date}
+        r = requests.post("https://data.police.uk/api/crimes-street/all-crime", params=payload)
+        if r.status_code == 404:
+            return ""
+        else:
+            res = r.json()
+            return res
 
 def store_cleaned_data(locations):
     r = []
     for location in locations:
         df_ = df[df['city'] == location]
-        data = fetch_data(df_.lat, df_.lng)
+        lat = float(df_.lat)
+        lng = float(df_.lng)
 
-        for x in data:
-            input = [location, x['category'], x['location']['latitude'], x['location']['longitude'], x['month']]
-            s = ""
-            for x in input:
-                s += x
+        p = int(df_.population)
 
-            p_key = hashlib.sha256(s.encode()).hexdigest()
-            input.insert(0, p_key)
+        for x in d[250:]:
+            data = fetch_data(lat, lng, x)
+            t = len(data)
+            if t != 0:
+                for i in data:
+                    c = i['category']
+                    m = x
+                    input = [location, c, str(lat), str(lng), m]
 
-            r.append(tuple(input))
+                    s = ""
+                    for j in input:
+                        s+=j
 
-    return r
+                    p_key = hashlib.sha256(s.encode()).hexdigest()
+                    input.insert(0, p_key)
+                    print(tuple(input))
+                    r.append(tuple(input))
+                
+        return r
+
 
 def store_dropdown_data(locations):
     r = []
     for location in locations:
         df_ = df[df['city'] == location]
-        data = fetch_data(df_.lat, df_.lng)
-        print(list(df_.lat)[0])
-        input = [location, str(list(df_.lat)[0]), str(list(df_.lng)[0]), str(len(data))]
+        lat = float(df_.lat)
+        lng = float(df_.lng)
 
-        s = ""
-        for x in input:
-            s += x
+        p = int(df_.population)
 
-        p_key = hashlib.sha256(s.encode()).hexdigest()
-        input.insert(0, p_key)
+        for x in d[250:]:
+            data = fetch_data(lat, lng, x)
+            t = len(data)
+            if t != 0:
+                f = t/p
+                m = x
+                input = [location, str(t), str(f), str(lat), str(lng), m]
 
-        r.append(tuple(input))
+                s = ""
+                for j in input:
+                    s+=j
 
+                p_key = hashlib.sha256(s.encode()).hexdigest()
+                input.insert(0, p_key)
+                r.append(tuple(input))
     return r
 
 def store_category_data(locations):
     r = []
     for location in locations:
         df_ = df[df['city'] == location]
-        data = fetch_data(df_.lat, df_.lng)
-        df_ = pd.DataFrame(data)
-        df_ = pd.DataFrame(df_['category'].value_counts())
+        lat = float(df_.lat)
+        lng = float(df_.lng)
 
-        i_ = []
-        for x in df_.index:
-            i_.append(x)
+        p = int(df_.population)
 
-        for i in range(len(df_)):
-            input = [location, i_[i], str(df_['category'][i])]
-            s = ""
-            for x in input:
-                s += x
+        for x in d[250:]:
+            data = fetch_data(lat, lng, x)
+            t = len(data)
+            if t != 0:
+                df_ = pd.DataFrame(data)
+                df_ = df_['category'].value_counts()
+                df_ = pd.DataFrame(df_)
+                
+                i_ = []
+                t_ = len(data)
+                if t != 0:
+                    i_ = []
+                    for j in df_.index:
+                        i_.append(j)
 
-            p_key = hashlib.sha256(s.encode()).hexdigest()
-            input.insert(0, p_key)
+                    
+                    for i in range(len(df_)):
+                        print(type(t), type(p))
 
-            r.append(tuple(input))
+                        t = df_['category'][i]
+                        f = t/p
+                        
+                        input = [location, i_[i], str(t), str(f), x]
+                        s = ""
+                        for x in input:
+                            s += x
 
+                        p_key = hashlib.sha256(s.encode()).hexdigest()
+                        input.insert(0, p_key)
+
+                        r.append(tuple(input))
+                    
     return r
 
 
@@ -93,13 +157,13 @@ r = store_cleaned_data(locations)
 for t in r:
     cursor.execute(insert_clean_data, t)
 
-insert_category_data = ("REPLACE INTO category_data (hash, location, category, count) VALUES (%s, %s, %s, %s)")
+insert_category_data = ("REPLACE INTO category_data (hash, location, category, total, fractional, month) VALUES (%s, %s, %s, %s, %s, %s)")
 
 r = store_category_data(locations)
 for t in r:
     cursor.execute(insert_category_data, t)
 
-insert_dropdown_data = ("REPLACE INTO dropdown_data (hash, location, lat, lng, count) VALUES (%s, %s, %s, %s, %s)")
+insert_dropdown_data = ("REPLACE INTO dropdown_data (hash, location, total, fractional, lat, lng, month) VALUES (%s, %s, %s, %s, %s, %s, %s)")
 
 r = store_dropdown_data(locations)
 for t in r:
@@ -118,12 +182,12 @@ cnx.close()
 
 # table dropdown_data
 
-# hash varchar, count varchar, lat varchar, lng varchar
+# hash varchar, total varchar, fractional varchar, lat varchar, lng varchar, month varchar
 
 
 # table category_data
 
-# location varchar, category varchar, count varchar
+# location varchar, category varchar, total varchar, fractional varchar, month varchar
 
 
 
